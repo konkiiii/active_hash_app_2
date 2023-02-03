14afebeeac0018207b02dcd5f05daab8bdeed5ed
konkiiii <kintaro0827@icloud.com>
2023-02-03 22:59:31 +0900

    firstcommit

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :prefectures_genre_id
+  validates :title, :text, presence: true
+
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefectures_genre_id, numericality: { other_than: 1, message: "can't be blank"}
+end
