a03d51dae2b14285e03d7efc76f32bf436168320
konkiiii <kintaro0827@icloud.com>
2023-02-04 14:37:57 +0900

    NameError解消
    uninitialized constant A::Bのエラー

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
-  belongs_to :prefectures_genre_id
+  belongs_to :prefectures_genre
   validates :title, :text, presence: true
 
   validates :genre_id, numericality: { other_than: 1, message: "can't be blank"}
